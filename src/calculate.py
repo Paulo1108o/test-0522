0ec3d70b9740bd43cffcfe686c429cf9ca247969
Paulo1108o <paulpaulo1108@gmail.com>
2026-05-22 11:48:30 +0800

    add mut_func

--- src/calculate.py
+++ src/calculate.py
@@ -1,6 +1,12 @@
 def add_func(a, b):
     return a + b
 
-def sub_func(a,b):
-    #temp=123
-    return a-b
+
+def sub_func(a, b):
+    # temp=123
+    return a - b
+
+
+def mut_func(a, b) -> float:
+    return "123"
+    # return a*b
